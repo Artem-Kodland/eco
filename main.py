ef7a8e38adb8d6f9d04660d77c678458aaee99ae
Artem-Kodland <134316152+Artem-Kodland@users.noreply.github.com>
2023-10-13 21:20:39 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+import os, random
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
